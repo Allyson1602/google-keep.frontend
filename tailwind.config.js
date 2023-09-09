2aef124de0ffbf64f47ec989107895f5e1ac0cce
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-09 12:55:05 -0300

    feat: definição do theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    borderColor: '#5f6368',
+    backgroundColor: '#202124',
+    fontFamily: {
+      sans: ['Roboto', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#fdbe563b',
+      },
+    },
   },
   plugins: [],
 }
