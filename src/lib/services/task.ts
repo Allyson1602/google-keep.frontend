94d6d84c0476d74b5e80375069dd61707bda0eba
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-09 19:39:55 -0300

    chore: definição de endpoints para tasks

--- src/lib/services/task.ts
+++ src/lib/services/task.ts
@@ -1,12 +1,28 @@
-import type { ITaskView } from "../models/task";
+import ApiRestClient, { type IResponse } from "../axios.config";
+import type { ITaskModel, ITaskView } from "../models/task";
 
 export interface ITaskService {
-    listTasksByUserId: (userId: number) => ITaskView[];
+    listTasksByUserId: (userId: number) => IResponse<ITaskModel[]>;
+    addTask: (userId: number, task: ITaskView) => IResponse<ITaskModel>;
+    updateTask: (id: number, task: ITaskModel) => IResponse<ITaskModel>;
+    deleteTask: (id: number) => IResponse<boolean>;
 }
 
-class TaskService implements ITaskService {
-    listTasksByUserId = (userId: number): ITaskView[] => {
-        return [];
+class TaskService extends ApiRestClient implements ITaskService {
+    listTasksByUserId = (userId: number): IResponse<ITaskModel[]> => {
+        return this.get(`/task/user/${userId}`);
+    };
+    
+    addTask = (userId: number, task: ITaskView): IResponse<ITaskModel> => {
+        return this.post(`/task/user/${userId}`, task);
+    };
+
+    updateTask = (id: number, task: ITaskModel): IResponse<ITaskModel> => {
+        return this.put(`/task/${id}`, task);
+    };
+
+    deleteTask = (id: number): IResponse<boolean> => {
+        return this.delete(`/task`, id);
     };
 }
 
