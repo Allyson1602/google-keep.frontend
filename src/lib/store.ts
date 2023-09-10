09e8db534b3cf7d8d2aa153fb25b0721f5d782d7
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-10 19:30:57 -0300

    chore: configuração para atualizar tarefa apenas quando houver alteração no checklist

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,73 +2,7 @@ import { writable } from 'svelte/store';
 import type { IListing } from './models/listing.model';
 
 function createListings() {
-	const { subscribe, update } = writable<IListing[]>([
-        {
-            id: 1,
-            title: 'Listing 1',
-            tasks: [
-                {
-                    id: 1,
-                    description: 'Description 1',
-                    done: false
-                },
-                {
-                    id: 2,
-                    description: 'Description 3',
-                    done: true
-                },
-                {
-                    id: 3,
-                    description: 'Description 4',
-                    done: false
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: 'Listing 2',
-            tasks: [
-                {
-                    id: 4,
-                    description: 'Description 2',
-                    done: true
-                }
-            ]
-        },
-        {
-            id: 3,
-            title: 'Listing 2',
-            tasks: [
-                {
-                    id: 4,
-                    description: 'Description 2',
-                    done: true
-                }
-            ]
-        },
-        {
-            id: 4,
-            title: 'Listing 2',
-            tasks: [
-                {
-                    id: 4,
-                    description: 'Description 2',
-                    done: true
-                }
-            ]
-        },
-        {
-            id: 5,
-            title: 'Listing 2',
-            tasks: [
-                {
-                    id: 4,
-                    description: 'Description 2',
-                    done: true
-                }
-            ]
-        }
-    ]);
+	const { subscribe, update } = writable<IListing[]>([]);
 
     function addListing(listing: IListing) {
         update(listings => [...listings, listing]);
